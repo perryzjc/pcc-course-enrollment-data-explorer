5260e6a77bfb33b241db5f8857509815d33b3588
Jingchao Zhong <perry.zjc@gmail.com>
2023-01-09 06:58:27 -0800

    Update test_storer.py solving the existence of .pytest_cache folder
    Previously, the test can only run after at least 1 test executed,
    since pytest automaically create a .pytest_cache folder after that.
    However, before that, the folder does not exists, thus my target base
    folder will not work correctly.

--- tests/data_sourcing/test_storer.py
+++ tests/data_sourcing/test_storer.py
@@ -15,10 +15,16 @@ import pytest
 from data_sourcing.storer import store_data
 
 
+ABSOLUTE_CWD = os.path.dirname(os.path.abspath(__file__))
+CACHE_DIR = os.path.join(ABSOLUTE_CWD, '.pytest_cache')
+
+
 @pytest.fixture
 def setup_data():
     sample_dict = {'32767': ['30', '12', '18', 'OPEN']}
     target_base_path = '.pytest_cache'
+    if not os.path.exists(CACHE_DIR):
+        os.mkdir(CACHE_DIR)
     return [sample_dict, target_base_path]
 
 
@@ -26,7 +32,7 @@ def test_curr_time1(setup_data):
     store_data(setup_data[0],
                time.struct_time((2023, 1, 13, 12, 34, 56, 0, 0, 0)),
                setup_data[1])
-    assert os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+    assert os.path.exists(os.path.join(ABSOLUTE_CWD,
                                        setup_data[1],
                                        '2023', '01', '13', '2023-01-13-12-34-56.csv'))
 
@@ -35,13 +41,6 @@ def test_curr_time2(setup_data):
     store_data(setup_data[0],
                time.struct_time((2026, 6, 6, 6, 6, 6, 0, 0, 0)),
                setup_data[1])
-    assert os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+    assert os.path.exists(os.path.join(ABSOLUTE_CWD,
                                         setup_data[1],
                                         '2026', '06', '06', '2026-06-06-06-06-06.csv'))
-    sample_dict = setup_data[0]
-    curr_time = time.struct_time((2023, 1, 13, 12, 34, 56, 0, 0, 0))
-    target_base_path = setup_data[1]
-    store_data(sample_dict, curr_time, target_base_path)
-    assert os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                       target_base_path,
-                                       '2023', '1', '13', '2023-1-13-12-34-56.csv'))
