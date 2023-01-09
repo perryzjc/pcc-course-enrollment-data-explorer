e3650080936dbe36302cdc92404702399274345a
Jingchao Zhong <perry.zjc@gmail.com>
2023-01-09 04:53:36 -0800

    Update main.py to call the data_sourcing package

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from parser import parse_html
-from storer import store_data
-from requester import get_html_of_course_web
+from data_sourcing.requester import get_html_of_course_web
+from data_sourcing.parser import parse_html
+from data_sourcing.storer import store_data
 import time
-import constants
+import data_sourcing.constants as constants
+import os
 
 
 while True:
     html = get_html_of_course_web()
     data = parse_html(html)
     curt_time = time.localtime(time.time())
-    store_data(data, curt_time)
+    store_data(data, curt_time, os.path.join(os.path.dirname(os.path.abspath(__file__)), constants.OUTPUT_DATA_SOURCING_FOLDER))
     time.sleep(constants.REQUEST_INTERVAL)
