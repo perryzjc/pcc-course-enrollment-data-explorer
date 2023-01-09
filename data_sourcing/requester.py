d5e29f8ac0a6c17f53584d92f76671fc35d4d29f
Jingchao Zhong <perry.zjc@gmail.com>
2023-01-09 04:52:50 -0800

    Make data sourcing related files as a package

--- data_sourcing/requester.py
+++ data_sourcing/requester.py
@@ -3,14 +3,14 @@
 """
 
 import requests
-import constants
+import data_sourcing.constants as const
 
 
 def get_html_of_course_web() -> str:
     """Return html data of PCC course schedule web
     """
     response = requests.request("POST",
-                                constants.ALL_COURSES_URL,
-                                headers=constants.HEADER,
-                                data=constants.POST_DATA)
+                                const.ALL_COURSES_URL,
+                                headers=const.HEADER,
+                                data=const.POST_DATA)
     return response.text
--- data_sourcing/storer.py
+++ data_sourcing/storer.py
@@ -12,11 +12,11 @@ Typical usage example:
 import csv
 import os
 import time
-import constants
 
 
 def store_data(data: dict[str, list[str, str, str]],
-               curr_time: time.struct_time
+               curr_time: time.struct_time,
+               target_folder_path: str
 ) -> None:
     """Store parsed data as a csv file
 
@@ -29,8 +29,12 @@ def store_data(data: dict[str, list[str, str, str]],
     Args:
         data: Parsed data from parser.parse_html()
         curr_time: time.struct_time object of the time the original html file is obtained
+        target_folder_path: base path of the folder to store the csv file. For example:
+
+        When target_folder_path is provided as pcc/output, the csv file could be stored
+        at pcc/output/2023/1/13/2023-1-13-12-34-56.csv
     """
-    target_folder = create_folder_needed_if_not_exists(curr_time)
+    target_folder = create_folder_needed_if_not_exists(curr_time, target_folder_path)
     file_name = time.strftime('%Y-%m-%d-%H-%M-%S', curr_time) + '.csv'
     file_path = os.path.join(target_folder, file_name)
     with open(os.path.join(file_path), 'w', newline='') as f:
@@ -40,7 +44,9 @@ def store_data(data: dict[str, list[str, str, str]],
             writer.writerow([crn] + data[crn])
 
 
-def create_folder_needed_if_not_exists(target_time: time.struct_time) -> str:
+def create_folder_needed_if_not_exists(target_time: time.struct_time,
+                                       target_folder_path: str
+) -> str:
     """Create folder structure needed and return the path of the resulting folder
 
     The folder is created based on the target_time parameter.
@@ -50,6 +56,7 @@ def create_folder_needed_if_not_exists(target_time: time.struct_time) -> str:
 
     Args:
         target_time: target time to create folder for
+        target_folder_path: base path of the folder to store the csv file.
 
     Returns:
         The path of the folder created based on the target time precise to the day.
@@ -78,7 +85,7 @@ def create_folder_needed_if_not_exists(target_time: time.struct_time) -> str:
         if not os.path.exists(folder_path):
             os.mkdir(folder_path)
 
-    year_folder_path = os.path.join(constants.OUTPUT_FOLDER, curr_year())
+    year_folder_path = os.path.join(target_folder_path, curr_year())
     create_folder_if_not_exists(year_folder_path)
     month_folder_path = os.path.join(year_folder_path, curr_month())
     create_folder_if_not_exists(month_folder_path)
