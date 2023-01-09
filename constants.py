59e6078f77fac53001a4a0326d60224c3538dc00
Jingchao Zhong <perry.zjc@gmail.com>
2023-01-09 02:26:54 -0800

    Add comments to each constant

--- constants.py
+++ constants.py
@@ -1,5 +1,19 @@
 """Store the constants used in the program
 """
 
+
+# OUTPUT_FOLDER specifies the folder where the output files are stored
 OUTPUT_FOLDER = 'output'
+# SAMPLE_FOLDER specifies the folder where the sample html files are stored
 SAMPLE_HTML_FOLDER = 'sample_html'
+# ALL_COURSES_URL is the url of the PCC course schedule web
+ALL_COURSES_URL = 'https://ssb-prod.ec.pasadena.edu/PROD/pw_psearch_sched.p_listthislist'
+# HEADER is the header of the request for PCC course schedule web
+HEADER = {
+    'Host': 'ssb-prod.ec.pasadena.edu',
+    'Upgrade-Insecure-Requests': '1',
+    'Origin': 'https://ssb-prod.ec.pasadena.edu',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Referer': 'https://ssb-prod.ec.pasadena.edu/PROD/pw_psearch_sched.p_search'}
+# POST_DATA is the data of the request for PCC course schedule web
+POST_DATA = "TERM=202330&TERM_DESC=Spring+2023&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_camp=dummy&sel_ism=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attrib=dummy&sel_schd=%25&sel_subj=%25&sel_crse=&sel_crn=&sel_title=&sel_ptrm=%25&sel_instr=%25&sel_attrib=%25&begin_hh=5&begin_mi=0&begin_ap=a&end_hh=11&end_mi=0&end_ap=p&aa=N&bb=N&ee=N&sel_sess=%25&sel_ism=%25&sel_camp=%25"
