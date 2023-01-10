5a2b87da8d9160d1a01b89f42fe4010bd7bbdc47
Jingchao Zhong <perry.zjc@gmail.com>
2023-01-09 23:25:27 -0800

    Change log message to make it more specific

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ in a fixed time interval.
 from data_sourcing.requester import get_html_of_course_web
 from data_sourcing.parser import parse_html
 from data_sourcing.storer import store_data
+from log import write_to_log_file, LOG_FILE_PATH
 import time
 import data_sourcing.constants as constants
 import os
@@ -18,5 +19,7 @@ while True:
     data = parse_html(html)
     curt_time = time.localtime(time.time())
     store_data(data, curt_time, os.path.join(os.path.dirname(os.path.abspath(__file__)), constants.OUTPUT_DATA_SOURCING_FOLDER))
-    print('successfully store a data file at time: ', time.strftime('%Y-%m-%d %H:%M:%S', curt_time))
+    msg = 'successfully store all course data as a csv file at time: ' + time.strftime('%Y-%m-%d %H:%M:%S', curt_time)
+    print(msg)
+    write_to_log_file(LOG_FILE_PATH, msg)
     time.sleep(constants.REQUEST_INTERVAL)
