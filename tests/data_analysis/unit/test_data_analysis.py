1e71e875aa355265b60385d739c2fcf480b6ee67
Jingchao Zhong <perry.zjc@gmail.com>
2023-01-09 21:32:47 -0800

    Add test file for data_analysis.py's get_all_course_status()

--- tests/data_analysis/unit/test_data_analysis.py
+++ tests/data_analysis/unit/test_data_analysis.py
@@ -8,6 +8,8 @@ from data_analysis.data_analysis import get_all_course_status
 SOURCE_HTML_DIR_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../data_sourcing/sample_html')
 SOURCE_HTML_FILE_1_COURSE_PATH = os.path.join(SOURCE_HTML_DIR_PATH, 'one_course_data.html')
 SOURCE_HTML_FILE_11_COURSES_PATH = os.path.join(SOURCE_HTML_DIR_PATH, 'few_courses_data.html')
+SOURCE_HTML_FILE_all_COURSES_PATH1 = os.path.join(SOURCE_HTML_DIR_PATH, 'all_courses_data.html')
+SOURCE_HTML_FILE_all_COURSES_PATH2 = os.path.join(SOURCE_HTML_DIR_PATH, 'all_courses_data2.html')
 
 
 @pytest.fixture
@@ -39,3 +41,30 @@ def test_parse_html_11_course(setup_data11):
     assert len(status_set) == 2
     assert 'OPEN' in status_set
     assert 'CLOSED' in status_set
+
+
+def output_all_courses_data():
+    """Output all courses status as a set to a file
+    >>> output_all_courses_data()
+    """
+    import csv
+    with open(SOURCE_HTML_FILE_all_COURSES_PATH1, 'r') as f:
+        html1 = f.read()
+    with open(SOURCE_HTML_FILE_all_COURSES_PATH2, 'r') as f:
+        html2 = f.read()
+    status_set1 = get_all_course_status(html1)
+    status_set2 = get_all_course_status(html2)
+    target_output_dir_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '.pytest_cache')
+    target_output_file_path1 = os.path.join(target_output_dir_path, 'all_courses_status1.csv')
+    target_output_file_path2 = os.path.join(target_output_dir_path, 'all_courses_status2.csv')
+    if not os.path.exists(target_output_dir_path):
+        os.mkdir(target_output_dir_path)
+    with open(target_output_file_path1, 'w') as f:
+        writer = csv.writer(f)
+        for status in status_set1:
+            writer.writerow([status])
+    with open(target_output_file_path2, 'w') as f:
+        writer = csv.writer(f)
+        for status in status_set2:
+            writer.writerow([status])
+
