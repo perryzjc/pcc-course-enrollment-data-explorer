fd66b0a59545b55ceb43b6e4115cd441b8e1f11e
Jingchao Zhong <perry.zjc@gmail.com>
2023-01-09 21:33:45 -0800

    Update test_parser.py with more accuarate tests

--- tests/data_sourcing/unit/test_parser.py
+++ tests/data_sourcing/unit/test_parser.py
@@ -3,10 +3,16 @@
 Testing strategy
     provide a sample html file of 1 course
     provide a sample html file of 11 courses
+    provide a sample html file of 35 courses
     provide a sample html file of all courses
+
+    partition on the expected course status (refer to data_sourcing.constants.COURE_STATUS_LIST):
+
+    Test large files by verifying the format of every course data in the dictionary
 """
 import os
 import pytest
+import data_sourcing.constants as const
 from data_sourcing.parser import parse_html
 
 ABSOLUTE_CWD = os.path.dirname(os.path.abspath(__file__))
@@ -22,6 +28,13 @@ def setup_html_1_course():
 @pytest.fixture
 def setup_html_11_courses():
     with open(os.path.join(ABSOLUTE_CWD, '../sample_html/few_courses_data.html'), 'r') as f:
+        html = f.read()
+    return html
+
+
+@pytest.fixture
+def setup_html_35_courses():
+    with open(os.path.join(ABSOLUTE_CWD, '../sample_html/few_courses_data2.html'), 'r') as f:
         html = f.read()
     return html
 
@@ -46,10 +59,16 @@ def test_parse_html_11_courses(setup_html_11_courses):
     helper_check_dict_format(data)
 
 
+def test_parse_html_35_courses(setup_html_35_courses):
+    data = parse_html(setup_html_35_courses)
+    assert len(data) == 35
+    assert data['38609'] == ['25', '5', '20', 'Restricted: See Counselor']
+    helper_check_dict_format(data)
+
+
 def test_parse_html_all_courses(setup_html_all_courses):
     data = parse_html(setup_html_all_courses)
-    assert len(data) == 2453
-    assert data['37201'] == ['20', '--', '0', 'CLOSED']
+    assert len(data) == 2750
     helper_check_dict_format(data)
 
 
@@ -66,5 +85,5 @@ def helper_check_dict_format(data: dict):
         assert len(data[d]) == 4
         assert data[d][0].isdigit()
         assert data[d][1].isdigit() or data[d][1] == '--'
-        assert data[d][2].isdigit()
-        assert data[d][3] == 'OPEN' or data[d][3] == 'CLOSED' or data[d][3] == 'Waitlisted'
+        assert data[d][2].isdigit() or data[d][2] == '--'
+        assert any(data[d][3] == s for s in const.COURSE_STATUS_LIST)
