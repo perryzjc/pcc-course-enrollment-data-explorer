389ac5b37e4670b93d33386c7b1d29b3aacd25ca
Jingchao Zhong <perry.zjc@gmail.com>
2023-03-20 01:01:04 -0700

    Enable requester to query data from certain term

--- backend/data_sourcing/requester.py
+++ backend/data_sourcing/requester.py
@@ -6,11 +6,18 @@ import requests
 from backend.config import config_data_sourcing as config
 
 
-def get_html_of_course_web() -> str:
+def get_html_of_course_web(term: str, year: str) -> str:
     """Return html data of PCC course schedule web
     """
+    if term not in config.TERM_MAP:
+        raise ValueError('Invalid term')
+    if not year.isdigit():
+        raise ValueError('Invalid year')
+    term_code = year + config.TERM_MAP[term]
+    term_desc = term + '+' + year
+    url = config.ALL_COURSES_URL.replace(config.DUMMY_TERM_CODE, term_code).replace(config.DUMMY_TERM_DESC, term_desc)
     response = requests.request("POST",
-                                config.ALL_COURSES_URL,
+                                url,
                                 headers=config.HEADER,
                                 data=config.POST_DATA)
     return response.text
